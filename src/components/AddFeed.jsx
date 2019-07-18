41ef3b3de51181a774cddda82d2138cb1aa7c7b0
Yaser Nicaragua <yanv1991@gmail.com>
2019-07-18 14:26:04 -0600

    add onEnter form submit

--- src/components/AddFeed.jsx
+++ src/components/AddFeed.jsx
@@ -5,8 +5,8 @@ import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import Grid from "@material-ui/core/Grid";
 
-import { createFeed } from '../actions'
-import { isValidURL } from '../helpers'
+import { createFeed } from "../actions";
+import { isValidURL } from "../helpers";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,37 +19,48 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     margin: theme.spacing(1),
-    minHeight: '3.5rem'
+    minHeight: "3.5rem"
   },
   truncate: {
     whiteSpace: "nowrap",
     overflow: "hidden",
     textOverflow: "ellipsis"
+  },
+  item: {
+    padding: theme.spacing(2),
+  },
+  form: {
+    display: "flex", width: "100%",
+    [theme.breakpoints.down("xs")]: {
+      flexDirection: 'column'
+    },
   }
 }));
 
-export const AddFeed = React.memo(
-  ({ onCreateFeed }) => {
-    const classes = useStyles();
-    const [url, setUrl] = useState("");
-  
-    const onChangeUrl = useCallback(e => {
-      setUrl(e.target.value);
-    }, []);
-  
-    const onSubmitUrl = useCallback(() => {
-        if(isValidURL(url)) {
-          onCreateFeed(url);
-          setUrl("")
-        }
-    }, [url]);
-  
-    return (
-      <Fragment>
-        <Grid item xs={11} sm={10} md={4}>
+export const AddFeed = React.memo(({ onCreateFeed }) => {
+  const classes = useStyles();
+  const [url, setUrl] = useState("");
+
+  const onChangeUrl = useCallback(e => {
+    setUrl(e.target.value);
+  }, []);
+
+  const onSubmitUrl = useCallback((e) => {
+    e.preventDefault();
+
+    if (isValidURL(url)) {
+      onCreateFeed(url);
+      setUrl("");
+    }
+  }, [url]);
+
+  return (
+    <Fragment>
+      <form className={classes.form} noValidate>
+        <Grid item xs={11} sm={10} md={4} className={classes.item}>
           <TextField
             id="filled-full-width"
-            inputProps={{ 'data-testid':"urlFeed" }}
+            inputProps={{ "data-testid": "urlFeed" }}
             style={{ margin: 8 }}
             placeholder="https://blog.safia.rocks/rss"
             fullWidth
@@ -62,8 +73,9 @@ export const AddFeed = React.memo(
             onChange={onChangeUrl}
           />
         </Grid>
-        <Grid item xs={11} sm={10} md={4}>
+        <Grid item xs={11} sm={10} md={4} className={classes.item}>
           <Button
+            type="submit"
             variant="contained"
             color="primary"
             className={classes.button}
@@ -73,9 +85,12 @@ export const AddFeed = React.memo(
             Add feed
           </Button>
         </Grid>
-      </Fragment>
-    );
-  }
-);
+      </form>
+    </Fragment>
+  );
+});
 
-export default connect(null, { onCreateFeed: createFeed })(AddFeed);
+export default connect(
+  null,
+  { onCreateFeed: createFeed }
+)(AddFeed);
