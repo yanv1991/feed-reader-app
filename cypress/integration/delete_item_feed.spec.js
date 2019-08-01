7f514129e3b9fe9bab43e4e00704bf87ed178ae3
Yaser Nicaragua <yanv1991@gmail.com>
2019-07-31 23:06:10 -0600

    add end 2 end

---
+++ cypress/integration/delete_item_feed.spec.js
@@ -0,0 +1,19 @@
+describe('Delete a feed item', () => {
+    it('Remove the item from list', () => {
+      cy.visit('http://localhost:3000');
+        
+      // we want to remove first item
+      cy.get('[data-testid="delete-0"]')
+        .click();
+    
+      // once button is clicked we should see a confirmation message
+      cy.contains('Are you sure you want to remove this item?')
+
+      // click on yes
+      cy.get('[data-testid="yes-button')
+        .click();
+        
+      // we should see a success message
+      cy.contains('Item removed successfully');
+    });
+  });
